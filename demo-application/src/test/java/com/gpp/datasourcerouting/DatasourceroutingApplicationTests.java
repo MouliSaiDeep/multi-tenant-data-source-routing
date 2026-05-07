e394f992a6870d0f7014a6ffd754b73390247085
Mouli Sai Deep <moulisaideep.get@gmail.com>
2026-05-07 09:45:00 +0530

    feat: implement demo application with user management and health checks

---
+++ demo-application/src/test/java/com/gpp/datasourcerouting/DatasourceroutingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpp.datasourcerouting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatasourceroutingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
